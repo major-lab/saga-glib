7d88b07f21cf529289443406d053fa158334c596
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2016-08-02 10:33:22 -0400

    Install files with 'install_data' and remove implicit '--target-glib'

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p ${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi
-
-install -m 0644                       \
-    ${MESON_BUILD_ROOT}/src/saga.vapi \
-    ${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi/saga-glib-1.0.vapi
